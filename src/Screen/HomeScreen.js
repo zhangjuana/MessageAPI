cf38ac6e308b380ad0b0396f00a04fcf6fa05822
Your Name <you@example.com>
2018-06-22 11:17:46 +0800

    first commit

--- src/Screen/HomeScreen.js
+++ src/Screen/HomeScreen.js
@@ -1,1 +1,25 @@
-import React,{Component} from 'react'
+import React, { Component } from 'react'
+import {
+    Toast,
+    NavBar,
+    ListView,
+    PullToRefresh
+} from 'antd-mobile';
+import React, { Component } from 'react'
+import { Text, View } from 'react-native'
+
+export default class HomeScreen extends Component {
+    render() {
+        return (
+            <div>
+                <NavBar mode="dark"
+                    leftContent={[
+                    <span>
+                        退出
+                    </span>]}
+                ></NavBar>
+            </div>
+
+        )
+    }
+}
